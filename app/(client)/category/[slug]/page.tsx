d3fc7cd223ab100bd4d6ff87f038b36131da03d9
awakening <awakrningjuniorgroup@gmail.com>
2026-04-08 10:59:11 +0100

    commiit category

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -10,31 +10,30 @@ interface PageProps {
   };
 }
 
-// Générer les chemins statiques pour chaque catégorie
 export async function generateStaticParams() {
   const categories: Category[] = await client.fetch(`*[_type == "category"]{slug}`);
   return categories.map((cat) => ({
-    slug: cat.slug.current,
+    slug: cat?.slug?.current,
   }));
 }
 
-// Fonction de page asynchrone qui reçoit params
 const CategoryPage = async ({ params }: PageProps) => {
   const slug = params.slug;
+  if (!slug) {
+    throw new Error("Slug parameter is missing");
+  }
 
-  // Récupérer toutes les catégories
   const categories: Category[] = await client.fetch(`*[_type == "category"]{_id, title, slug}`);
 
-  // Récupérer les produits de la catégorie slug
   const productsQuery = `
     *[_type == 'product' && references(*[_type == "category" && slug.current == $slug]._id)] | order(name asc){
       ...,
       "categories": categories[]->title
     }
   `;
+
   const initialProducts: Product[] = await client.fetch(productsQuery, { slug });
 
-  // Passer les données au composant client
   return (
     <CategoryProducts
       categories={categories}
