e8000f2623c1acef7f27b0e826b5ffbb040b07d3
awakening <awakrningjuniorgroup@gmail.com>
2026-04-08 11:22:10 +0100

    commit

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,52 +1,28 @@
-// app/category/[slug]/page.tsx
-
-import { Category, Product } from "@/sanity.types";
-import { client } from "@/sanity/lib/client";
 import CategoryProducts from "@/components/CategoryProducts";
+import Container from "@/components/Container";
+import { Title } from "@/components/text";
+import { getCategories } from "@/sanity/queries";
+import React from "react";
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export async function generateStaticParams() {
-  const categories: Category[] = await client.fetch(`*[_type == "category"]{slug}`);
-  return categories
-  
-    .filter(cat => cat.slug?.current)
-    .map(cat => ({
-      slug: cat.slug!.current,
-    }));
-
-   
-}
-
-
-const CategoryPage = async ({ params }: PageProps) => {
-  const slug = params.slug;
-  if (!slug) {
-    throw new Error("Slug parameter is missing");
-  }
-
-  const categories: Category[] = await client.fetch(`*[_type == "category"]{_id, title, slug}`);
-
-  const productsQuery = `
-    *[_type == 'product' && references(*[_type == "category" && slug.current == $slug]._id)] | order(name asc){
-      ...,
-      "categories": categories[]->title
-    }
-  `;
-   
-
-  const initialProducts: Product[] = await client.fetch(productsQuery, { slug });
-
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const categories = await getCategories();
+  const { slug } = await params;
   return (
-    <CategoryProducts
-      categories={categories}
-      initialSlug={slug}
-      initialProducts={initialProducts}
-    />
+    <div className="py-10">
+      <Container>
+        <Title>
+          Products by Category:{" "}
+          <span className="font-bold text-green-600 capitalize tracking-wide">
+            {slug && slug}
+          </span>
+        </Title>
+        <CategoryProducts categories={categories} slug={slug} />
+      </Container>
+    </div>
   );
 };
 
